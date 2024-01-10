43d5f805fb8fa3714bf2c8deafc00f1a7bd6ecd0
devopswithcloud <65442863+devopswithcloud@users.noreply.github.com>
2024-01-10 08:27:13 +0530

    secondchange

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -1,22 +1,31 @@
 import com.i27academy.builds.Calculator
-Calculator calculator = new Calculator(this)
-pipeline {
-    agent any 
-    stages {
-        stage ('AdditionStage') {
-            steps {
-                script {
-                    echo "Printing Sum of 2 numbers"
-                    println calculator.add(3,4)
-                    //sub(10,9) // 10-9 = 1
-                    //sub(9,10) //9-10 = -1
+
+def call(Map pipelineParams){
+    library("com.i27academy.slb")
+    Calculator calculator = new Calculator(this)
+    pipeline {
+        agent any 
+        environment {
+            APP_NAME = "${pipelineParams.appName}"
+        }
+        stages {
+            stage ('AdditionStage') {
+                steps {
+                    script {
+                        echo "Printing Sum of 2 numbers"
+                        println calculator.add(3,4)
+                        //sub(10,9) // 10-9 = 1
+                        //sub(9,10) //9-10 = -1
+                        echo "Microservice Name is: ${APP_NAME}"
+                    }
                 }
             }
-        }
-        stage ('Nothing') {
-            steps {
-                echo "Printing text not from groovy files"
+            stage ('Nothing') {
+                steps {
+                    echo "Printing text not from groovy files"
+                }
             }
         }
     }
 }
+
